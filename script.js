1407454ed865b174bb4c729890591f078e4e307b
Chantee Hoarau <chantee.hoarau123@gmail.com>
2023-05-11 09:08:36 +0200

    Add a function that switches between players and the name of the player playing

--- script.js
+++ script.js
@@ -6,3 +6,36 @@ const player = (sign) => {
     }
     return {getSign};
 };
+
+const gameBoard = (() => {
+    let playerX = player("X");
+    let playerO = player("O");
+    let currentPlayer = playerX.getSign();
+    const output = document.getElementById('output');
+    const gridItem = document.getElementsByClassName('grid-item');
+    let tieCount = 0;
+    let gameActive = true;
+    const resetBtn = document.querySelector('#reset');
+    const output2 = document.getElementById('output2');
+
+
+ const displayPlayer = () => {
+    for (let i = 0; i < gridItem.length; i++) {
+        gridItem[i].addEventListener('click', () => {
+            if(gridItem[i].innerHTML === '' && gameActive === true) {
+            gridItem[i].innerHTML = currentPlayer;
+            currentPlayer = currentPlayer === playerX.getSign() ? playerO.getSign() : playerX.getSign();
+            tieCount++;
+            checkWinner();
+        }
+        if(currentPlayer == playerX.getSign()){
+            output.innerHTML = 'Player: X';
+            
+        }else{
+            output.innerHTML = 'Player: O';
+           
+        }
+    })
+}
+}
+})
