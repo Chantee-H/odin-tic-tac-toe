c1a1a2ac33f0c9ec19c426b240476926e1bb3898
Chantee Hoarau <chantee.hoarau123@gmail.com>
2023-05-11 09:10:49 +0200

    Add a function to check for a winner or a tie

--- script.js
+++ script.js
@@ -52,4 +52,37 @@ const reset = () => {
     })
 }
 }
+
+const checkWinner = () => {
+    let win = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+    
+    for (let i = 0; i < win.length; i++) {
+        let combination = win[i];
+        let a = gridItem[combination[0]].textContent;
+        let b = gridItem[combination[1]].textContent;
+        let c = gridItem[combination[2]].textContent;
+
+        if (a !== '' && a === b && b === c) {
+            output2.innerHTML = `Player ${a} Wins!`;
+            gameActive = false;
+            gridItem[combination[0]].style.backgroundColor = '#60bb60';
+            gridItem[combination[1]].style.backgroundColor = '#60bb60';
+            gridItem[combination[2]].style.backgroundColor = '#60bb60';
+       }
+       if(tieCount == 9){
+        output2.innerHTML = 'Tie!';
+       }
+    }
+   
+}
 })
+
